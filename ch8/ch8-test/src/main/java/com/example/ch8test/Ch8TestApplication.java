59f56d4633d50982df0ad6973694946c7d6b8073
“designexit” <didmk.lab@gmail.com>
2023-10-06 09:22:09 +0900

    [1006 9:21]ch8 test

---
+++ ch8/ch8-test/src/main/java/com/example/ch8test/Ch8TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch8TestApplication.class, args);
+    }
+
+}
---
+++ ch8/ch8-test/src/test/java/com/example/ch8test/Ch8TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch8test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch8TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
